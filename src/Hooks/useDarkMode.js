220a7e97778c5f3439797d0774cf65a31f318d99
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-09-30 17:58:23 -0400

    Project Dark Mode MVP met commitment

---
+++ src/Hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import useLocalStorage from "./useLocalStorage"
+import { useEffect } from "react"
+
+ function useDarkMode (initialValue){
+    const [darkMode, setDarkMode]=useLocalStorage(initialValue)
+
+    useEffect (()=>{
+        if(darkMode === true){
+            document.body.classList.add('dark-mode')
+        }else{
+            document.body.classList.remove('dark-mode')  
+        }
+    },[darkMode])
+    return [darkMode,setDarkMode]
+}
+export default useDarkMode;
